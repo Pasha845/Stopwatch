bc83ad4ce99bf853b4a6b66d135ebc40906b4bf5
Павел Горулев <pasha.gorulev@gmail.com>
2023-04-23 16:31:16 +0300

    Улучшения

--- js/timer.js
+++ js/timer.js
@@ -1,46 +1,52 @@
 window.onload = function () {
+  var tens = 00;
   var seconds = 00; 
-  var tens = 00; 
-  var appendTens = document.getElementById("tens")
-  var appendSeconds = document.getElementById("seconds")
+  var minutes = 00; 
+  var hours = 00;
+  var appendSeconds = document.getElementById("seconds");
+  var appendMinutes = document.getElementById("minutes");
+  var appendHours = document.getElementById("hours");
   var buttonStart = document.getElementById('button-start');
-  var buttonStop = document.getElementById('button-stop');
   var buttonReset = document.getElementById('button-reset');
   var Interval;
+  var flag = false;
 
   buttonStart.onclick = function() {
-    clearInterval(Interval);
-    Interval = setInterval(startTimer, 10);
-  }
-  
-  buttonStop.onclick = function() {
-    clearInterval(Interval);
+    if(flag == false) {
+      clearInterval(Interval);
+      Interval = setInterval(startTimer, 10);
+      flag = true;
+    }
+    else {
+      clearInterval(Interval);
+      flag = false;
+    }
   }
 
   buttonReset.onclick = function() {
     clearInterval(Interval);
-    tens = "00";
   	seconds = "00";
-    appendTens.innerHTML = tens;
+    minutes = "00";
+    hours = "00";
   	appendSeconds.innerHTML = seconds;
+    appendMinutes.innerHTML = minutes;
+    appendHours.innerHTML = hours;
   }
   
   function startTimer() {
-    tens++; 
-    
-    if(tens <= 9) {
-      appendTens.innerHTML = "0" + tens;
-    }
-    
-    if (tens > 9) {
-      appendTens.innerHTML = tens;
-    } 
+    tens++;
     
     if (tens > 99) {
-      seconds++;
-      appendSeconds.innerHTML = "0" + seconds;
-      tens = 0;
-      appendTens.innerHTML = "0" + 0;
+      if (seconds == 59) {
+        minutes++
+        seconds = 0
+        appendMinutes.innerHTML = "0" + minutes;
+      }
+      else {
+        seconds++;
+        appendSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+      }
     }
     
     if (seconds > 9) {
@@ -48,3 +54,7 @@ window.onload = function () {
     }
   }
 }
+
+document.querySelector('.timer__subbtn').addEventListener('click', function(e) {
+  e.target.closest('.timer__subbtn').classList.toggle('timer__btn-toggle');
+})
